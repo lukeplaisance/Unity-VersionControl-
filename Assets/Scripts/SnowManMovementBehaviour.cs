692e771e4d02ccece03c204bd5950d0a686e08b0
lukeplaisance17 <31013260+lukeplaisance17@users.noreply.github.com>
2018-06-19 11:46:37 -0500

    added movement script

---
+++ Assets/Scripts/SnowManMovementBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowManMovementBehaviour : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        var jump = Input.GetAxis("Vertical");
+        var movement = new Vector3(0, jump, 0);
+        rb.AddForce(movement * speed);
+	}
+}
